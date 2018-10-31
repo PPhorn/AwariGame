3694cdd5ed8a4c132d48a04de6879d3db5f9cb15
Inge Ingejonas Jonas <ingejonas@Inges-MBP.parknet.dk>
2018-10-31 13:56:48 +0100

    fs fil opdateret.

---
+++ AwariLib.fs
@@ -0,0 +1,46 @@
+
+/// Prints the board
+let printBoard (b: board) =
+    for i = 12 downto 7 do
+        printf "%4i" b.[i]
+    printfn ""
+    printf "%i %25i\n" b.[6] b.[13]
+    for i = 0 to 5 do
+        printf "%4i" b.[i]
+    printfn ""
+
+/// Checks whether pit is player's home
+let isHome (b: board) (p: player) (i: pit) : bool =
+    match p with
+    | Player1 when i = 6 -> true
+    | Player2 when i = 13 -> true
+    | _ -> false
+
+let turn (b : board) (p : player) : board =
+  let rec repeat (b: board) (p: player) (n: int) : board =
+    printBoard b
+    let str =
+      if n = 0 then
+        sprintf "Player %A's move? " p
+      else
+        "Again? "
+    let i = getMove b p str
+    let (newB, finalPitsPlayer, finalPit)= distribute b p i
+    if not (isHome b finalPitsPlayer finalPit)
+       || (isGameOver b) then
+      newB
+    else
+      repeat newB p (n + 1)
+  repeat b p 0
+
+let rec play (b : board) (p : player) : board =
+  if isGameOver b then
+    b
+  else
+    let newB = turn b p
+    let nextP =
+      if p = Player1 then
+        Player2
+      else
+        Player1
+    play newB nextP
