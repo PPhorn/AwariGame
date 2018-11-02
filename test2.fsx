9dd10bd8b04db8d1aae07706f6cf4456c8a797b8
Inge Ingejonas Jonas <ingejonas@Inges-MacBook-Pro.local>
2018-11-02 13:03:03 +0100

    test2

--- test2.fsx
+++ test2.fsx
@@ -3,15 +3,13 @@ let board2 = [|0; 0; 3; 0; 0; 0; 10; 3; 3; 3; 3; 3; 3; 0;|]
 type player = Player1 | Player2
 type board = int array
 type pit = int
-let player = Player2
 
-let rec getMove (b:board) (p:player) (q:string) : pit =
-    printfn "Vælg et felt fra 1-6"
-    let n = int (System.Console.ReadLine ())
-    if (1 <= n && n <= 6) then
-      match p with
-      | Player1 -> n-1
-      | Player2 -> n+6
-    else
-      getMove b p ""
-printfn "%A" (getMove board2 Player2 "")
+
+let rec distribute (b:board) (p:player) (i:pit) : board * player * pit =
+let j = i + 1
+for i = j to i + A.[i] do
+  A.[j] = A.[j] + 1
+  b
+
+
+printfn "%A" (distribute board2 Player1 getMove)
