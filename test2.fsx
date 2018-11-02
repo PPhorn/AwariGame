f508c313f48c8235910c087802b962994f7c57f3
Pernille Horn <phorn@Pernilles-MacBook-Pro-2.local>
2018-11-02 14:14:48 +0100

    update af test 2 med udkast til distribute

--- test2.fsx
+++ test2.fsx
@@ -5,11 +5,25 @@ type board = int array
 type pit = int
 
 
+//Checks pit opposit of finalPit
+let checkOpp (b:board) (i: pit) : bool =
+    let Opps = (b.Length - 2) - i
+    (b.[Opps] <> 0)
+
+
 let rec distribute (b:board) (p:player) (i:pit) : board * player * pit =
-let j = i + 1
-for i = j to i + A.[i] do
-  A.[j] = A.[j] + 1
-  b
+  let j = i + 1
+  let Opps = (b.Length - 2) - i
+  for j = j to i + b.[i] do
+    b.[j] <- (b.[j] + 1)
+  if (checkOpp b i) then
+    match p with
+    | Player1 -> b.[6] <- b.[6] + b.[Opps] + b.[i+b.[i]]
+    | Player2 -> b.[13] <- b.[13] + b.[Opps] + b.[i+b.[i]]
+  b.[i+b.[i]] <- 0
+  b.[Opps] <- 0
+  b.[i] <- 0
+  (b, p, i)
 
 
-printfn "%A" (distribute board2 Player1 getMove)
+printfn "%A" (distribute board2 Player1 2)
