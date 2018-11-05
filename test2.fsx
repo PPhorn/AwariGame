dcc681bc44abafef2db73dbe16cf8490fa900e59
Christoffer <Christofferoeh@gmail.com>
2018-11-05 11:42:31 +0100

    SLAMKODEDISTRIBUTE

--- test2.fsx
+++ test2.fsx
@@ -1,5 +1,5 @@
 
-let board2 = [|0; 0; 4; 0; 0; 0; 10; 3; 3; 4; 0; 3; 3; 20;|]
+let board2 = [|4; 4; 4; 0; 0; 0; 10; 3; 3; 3; 3; 3; 0; 20;|]
 type player = Player1 | Player2
 type board = int array
 type pit = int
@@ -30,12 +30,23 @@ let finalPitPlayer (i: pit) : player =
 //val distribute : b:board -> p:player -> i:pit -> board * player * pit
 
 let rec distribute (b:board) (p:player) (i:pit) : board * player * pit =
-  let j = i + 1
-  let finalPit = i + b.[i]
-  let Opps = (b.Length - 2) - finalPit
-  for j = j to i + b.[i] do
-    b.[j] <- (b.[j] + 1)
+  let mutable j = i + 1
+  ///Let k be the number of pits to distribute
+  let mutable k = b.[i]
+  while k > 0 do
+    if (j < 14) then
+      b.[j] <- (b.[j] + 1)
+    if (j = 14) then
+      printfn "J <- 0"
+      j <- 0
+    else
+      j <- j + 1
+      printfn "J+1, %A" j
+    k <- k - 1
+    printfn "K-1"
+  let finalPit = j
   if (checkOpp b finalPit) && b.[finalPit] = 1 then
+    let Opps = (b.Length - 2) - finalPit
     match p with
     | Player1 -> b.[6] <- b.[6] + b.[Opps] + b.[finalPit]
     | Player2 -> b.[13] <- b.[13] + b.[Opps] + b.[finalPit]
@@ -45,4 +56,4 @@ let rec distribute (b:board) (p:player) (i:pit) : board * player * pit =
   (b, (finalPitPlayer finalPit), finalPit)
 
 
-printfn "%A" (distribute board2 Player2 )
+printfn "%A" (distribute board2 Player2 10 )
