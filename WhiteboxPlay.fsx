61a49bd8f088f22e37d1b5d770ea4e963674d036
Inge Ingejonas Jonas <ingejonas@Inges-MacBook-Pro.local>
2018-11-20 16:05:04 +0100

    play

--- WhiteboxPlay.fsx
+++ WhiteboxPlay.fsx
@@ -29,18 +29,18 @@ let rec play (b : board) (p : player) : board =
       printfn "%s" (addColor "Game over. The winner is Player 2" gameOverColor)
     b
   else
-    let newB = turn b p
+    let newB = boardtest10
     let nextP =
       if p = Player1 then
         Player2
       else
         Player1
-    play newB nextP
+    printfn "%A" (play newB nextP)
 
 (*TESTING AF play*)
 printfn "\nWhitebox testing of the function play:"
-printfn "Branch 1a: if isGameOver = true output: printBoard + The winner is Player 1 - %b" (play boardtest1 Player1 = [|0; 0; 0; 0; 0; 0; 10; 3; 3; 3; 3; 3; 3; 0;|])
-printfn "Branch 1b: if isGameOver = true output: printBoard + It's a tie - %b" (play boardtest0 Player1 = [|0; 0; 0; 0; 0; 0; 18; 0; 0; 0; 0; 0; 0; 18;|])
-printfn "Branch 1c: if isGameOver = true output: printBoard + The winner is Player 2 - %b" (play boardtest14 Player2 = [|0; 0; 0; 0; 0; 0; 18; 5; 0; 0; 0; 0; 8; 5;|])
-printfn "Branch 2a: if isGameOver = false output: [|3; 3; 3; 3; 3; 3; 0; 3; 3; 3; 3; 3; 3; 0;|] - %b" (play boardtest10 Player2 = [|3; 3; 3; 3; 3; 3; 0; 3; 3; 3; 3; 3; 3; 0;|])
+printfn "Branch 1a: if isGameOver = true. Exp. output: printBoard + The winner is Player 1 - %b" (play boardtest1 Player1 = [|0; 0; 0; 0; 0; 0; 10; 3; 3; 3; 3; 3; 3; 0;|])
+printfn "Branch 1b: if isGameOver = true. Exp. output: printBoard + It's a tie - %b" (play boardtest0 Player1 = [|0; 0; 0; 0; 0; 0; 18; 0; 0; 0; 0; 0; 0; 18;|])
+printfn "Branch 1c: if isGameOver = true. Exp. output: printBoard + The winner is Player 2 - %b" (play boardtest14 Player2 = [|0; 0; 0; 0; 0; 0; 18; 5; 0; 0; 0; 0; 8; 5;|])
+printfn "Branch 2a: if isGameOver = false. Exp. output: [|3; 3; 3; 3; 3; 3; 0; 3; 3; 3; 3; 3; 3; 0;|] - %b" (play boardtest10 Player2 = [|3; 3; 3; 3; 3; 3; 0; 3; 3; 3; 3; 3; 3; 0;|])
 // printfn "Branch 2b: if isGameOver = false output: printBoard + The winner is Player 2 - %b" (play  = Player1)
